ed006a48dff96adeedb5f09a2aed3302a43ffd78
codewithles <168603675+codewithles@users.noreply.github.com>
2024-05-05 23:29:10 +0200

    create_pyscript_project.bat

--- create_pyscript_project.bat
+++ create_pyscript_project.bat
@@ -4,7 +4,7 @@ mkdir pyscript-offline && cd pyscript-offline
 echo "{}" > ./package.json
 mkdir public && mkdir "./public/css" && mkdir "./public/js" && mkdir "./public/img"
 echo. > "./public/css/style.css" && echo. > "./public/js/app.js"
-(echo "visit my GitHub https://github.com/codewithles/ for more automation and other projects" &&  echo ^<script type="module" src="/pyscript/core.js"^>^</script^> && echo ^<link rel="stylesheet" href="/pyscript/core.css"^>) > ./public/index.html
+(echo "visit my GitHub https://github.com/codewithles/ for more automation and other projects" &&  echo ^<script type="module" src="./pyscript/core.js"^>^</script^> && echo ^<link rel="stylesheet" href="./pyscript/core.css"^>) > ./public/index.html
 npm install @pyscript/core && echo cp -R ./node_modules/@pyscript/core/dist ./public/pyscript | powershell.exe
 
 PAUSE
